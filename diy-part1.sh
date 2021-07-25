3e67699c41295164b552c9ac4b5416ba34281653
小丶浣 <78002376+xiaiohuan@users.noreply.github.com>
2021-07-25 11:43:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone https://github.com/xiaiohuan/openwrt-packages.git package/openwrt-packages
+git clone https://github.com/xiaiohuan/small.git  package/openwrt-small
